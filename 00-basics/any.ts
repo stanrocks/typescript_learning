92953dd3211c8a7cead1ed72f6dd4153079df921
stanrocks <25880632+stanrocks@users.noreply.github.com>
2022-12-05 01:37:57 +0600

    type any, type inference

---
+++ 00-basics/any.ts
@@ -0,0 +1,14 @@
+const myComplicatedData: any = "I'm gonna be complicated!";
+// this kinda defeats the purpose of TS
+// so genereally you don't want to use it
+
+// the any type
+let thing: any = "hello";
+thing = 1; // valid
+thing = false; // also valid
+thing(); // it's a boolean, not a function, but TS will not show that it's not valid
+thing.toUpperCase(); // no such method for boolean, but TS will not show that it's not valid
+
+let thing2 = "hello";
+thing2(); // TS says that it's not valid, cause it's a string, not a function, string is not callable
+thing2.lsdfjkkjsf(); // TS says it's not valid - no such method for a string
--- 00-basics/variables.ts
+++ 00-basics/variables.ts
@@ -1,12 +1,27 @@
-let movieTitle: string = "Amadeus";
+let movieTitle: string = "Amadeus"; // redundant
 movieTitle = "Arrival";
 movieTitle = 9; // not valid
-movieTitle.toUpperCase(); // valid since TS knows that string's method is available
+movieTitle.toUpperCase(); // valid cause TS knows that string's method is available
 
-let numCatLives: number = 9;
+let numCatLives: number = 9; // redundant
 numCatLives += 1; // valid
 numCatLives = "zero"; // not valid
 
-let isGameOver: boolean = false;
+let isGameOver: boolean = false; // redundant
 isGameOver = true;
 isGameOver = "true"; // not valid
+
+let nothing: null = null; // valid (but redundant?)
+let foo: undefined = undefined; // valid (but redundant?)
+
+// Type Inference
+let x = 27; // no need to define type here - redundant
+x = "twenty seven"; // not valid cause TS already knows that type is a number
+
+let tvShow = "Breaking Bad"; // TS knows that it's a string
+tvShow = "Better Call Saul"; // valid
+tvShow = false; // not a valid type
+
+let isFunny = false; // TS knows that it's boolean
+isFunny = true; // valid
+isFunny = "asd"; // not a valid type
