15c2d533a077d196b247875214a81315bf992752
stanrocks <25880632+stanrocks@users.noreply.github.com>
2022-12-19 00:27:15 +0600

    type assertions with the DOM

--- 00-basics/typeAssertion.ts
+++ 00-basics/typeAssertion.ts
@@ -7,9 +7,22 @@ const numChars = mystery.length; // invalid - no method length for unknown type 
 const numChars2 = (mystery as string).length; // valid
 
 // ================================================
-
 // TS infers a type of HTMLElement
 const myPic = document.querySelector("profile-image");
 
 // But we know a more specific type, so let's assert it!
 const myPic2 = document.querySelector("profile-image") as HTMLImageElement;
+
+// ================================================
+// Type assertions with the DOM
+
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+// We can see type using console.dir(input) // HTMLInputElement
+// And now when input is defined as HTMLInputElement - we can use methods like input.value:
+alert(input.value);
+
+// Alternative syntax for type assertions (instead of "... as ... ")
+// by using angle brackets:
+alert((<HTMLInputElement>input).value);
+// but it's less common
+// it doesn't work with JSX i.e. React
