fcf646a99606cabbacf48a03d414e3817e40e003
stanrocks <25880632+stanrocks@users.noreply.github.com>
2022-12-19 02:17:25 +0600

    todolist boilerplate

--- 01-miniproject/src/index.ts
+++ 01-miniproject/src/index.ts
@@ -1,1 +1,18 @@
-console.log("WOOO!");
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("#todoform")! as HTMLFormElement;
+const list = document.getElementById("todolist")!;
+
+function handleSubmit(e: SubmitEvent) {
+	e.preventDefault();
+	const newTodoText = input.value;
+	const newLI = document.createElement("li");
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	newLI.append(newTodoText);
+	newLI.append(checkbox);
+	list.append(newLI);
+	input.value = "";
+}
+
+form.addEventListener("submit", handleSubmit);
