07743f4d26b2d0114c3cb1cfbaac9e24e9d0c798
stanrocks <25880632+stanrocks@users.noreply.github.com>
2022-12-21 22:11:53 +0600

    annotating classes in ts

--- 02-classes/dist/index.js
+++ 02-classes/dist/index.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("it's !!! working");
+// Annotating classes in TS
+class Player {
+    constructor(firstName, lastName) {
+        this.first = firstName;
+        this.last = lastName;
+    }
+}
+const elton = new Player("Elton", "Steele");
--- 02-classes/src/index.ts
+++ 02-classes/src/index.ts
@@ -1,1 +1,12 @@
-console.log("it's !!! working");
+// Annotating classes in TS
+class Player {
+	// Annotating here prop types
+	first: string;
+	last: string;
+	constructor(firstName: string, lastName: string) {
+		this.first = firstName;
+		this.last = lastName;
+	}
+}
+
+const elton = new Player("Elton", "Steele");
