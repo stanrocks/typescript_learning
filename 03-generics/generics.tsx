00788e32ed9de95016ef8b7124d2df4748f93125
stanrocks <25880632+stanrocks@users.noreply.github.com>
2022-12-27 23:29:25 +0600

    generics in JSX (i.e. React)

---
+++ 03-generics/generics.tsx
@@ -0,0 +1,22 @@
+// Working with generics in JSX (i.e. React) in tsx-files
+
+// <> are used for components
+// and also they are used in TS generics
+
+// when we write a function like this - its totally valid. It's able to parse and understand that this <T> is a type parameter, not a react component:
+function getRandomElement<T>(list: T[]): T {
+	const randomIndex = Math.floor(Math.random() * list.length);
+	return list[randomIndex];
+}
+
+// but it's going crazy when using arrow functions:
+// const getRandomElement2 = <T>(list: T[]): T => {
+// 	const randomIndex = Math.floor(Math.random() * list.length);
+// 	return list[randomIndex];
+// }
+
+// to fix it we need to add a trailing comma inside <>:
+const getRandomElement3 = <T,>(list: T[]): T => {
+	const randomIndex = Math.floor(Math.random() * list.length);
+	return list[randomIndex];
+};
