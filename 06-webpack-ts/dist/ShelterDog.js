2bac1815c73ec8ed9f2ca4bfd0432b87b1ba4cfb
stanrocks <25880632+stanrocks@users.noreply.github.com>
2023-01-13 04:18:53 +0600

    webpack installed

--- 06-webpack-ts/dist/ShelterDog.js
+++ 06-webpack-ts/dist/ShelterDog.js
@@ -1,7 +1,7 @@
-import Dog from "./Dog.js";
+import Dog from "./Dog";
 export default class ShelterDog extends Dog {
-	constructor(name, breed, age, shelter) {
-		super(name, breed, age);
-		this.shelter = shelter;
-	}
+    constructor(name, breed, age, shelter) {
+        super(name, breed, age);
+        this.shelter = shelter;
+    }
 }
--- 06-webpack-ts/dist/utils.js
+++ 06-webpack-ts/dist/utils.js
@@ -1,13 +1,12 @@
-// without webpach this will not work after transpilig to JS, because whatever there is in "dist" - it will not have access to lodash node module. We have to add lodash to dist using webpack
 import _ from "lodash";
 export function add(x, y) {
-	// creating some artificial dependency
-	// just to see more activities in network tab in chrome dev tools
-	return _.add(x, y);
+    // creating some artificial dependency
+    // just to see more activities in network tab in chrome dev tools
+    return _.add(x, y);
 }
 export function multiply(x, y) {
-	return x * y;
+    return x * y;
 }
 export function divide(x, y) {
-	return x / y;
+    return x / y;
 }
