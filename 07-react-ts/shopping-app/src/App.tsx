7fdc160a65891b22700831c3bbefb49074373df0
stanrocks <25880632+stanrocks@users.noreply.github.com>
2023-01-19 05:54:11 +0600

    ts react first component

--- 07-react-ts/shopping-app/src/App.tsx
+++ 07-react-ts/shopping-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Greeter from "./components/Greeter";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello world!</h1>
+			<Greeter />
 		</div>
 	);
 }
