7fac6a8b8d338343ee6e3ed671e095cffc3092e2
stanrocks <25880632+stanrocks@users.noreply.github.com>
2023-01-19 03:06:23 +0600

    webpack dev server

--- 06-webpack-ts/webpack.config.js
+++ 06-webpack-ts/webpack.config.js
@@ -2,8 +2,15 @@
 const path = require("path");
 
 module.exports = {
+	mode: "development",
 	entry: "./src/index.ts",
 	devtool: "inline-source-map",
+	// https://webpack.js.org/configuration/dev-server/#devserver
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "./"),
+		},
+	},
 	module: {
 		rules: [
 			{
@@ -19,5 +26,6 @@ module.exports = {
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
+		publicPath: "/dist",
 	},
 };
