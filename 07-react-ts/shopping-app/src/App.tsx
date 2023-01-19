f9426e87e9fd1471e98d27ce828d43f939b45b27
stanrocks <25880632+stanrocks@users.noreply.github.com>
2023-01-20 05:24:11 +0600

    updating the state

--- 07-react-ts/shopping-app/src/App.tsx
+++ 07-react-ts/shopping-app/src/App.tsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import ShoppingList from "./components/ShoppingList";
 import ShoppingListForm from "./components/ShoppingListForm";
 import Item from "./models/item";
+import { v4 as getId } from "uuid";
 import "./App.css";
 
 function App() {
 	const [items, setItems] = useState<Item[]>([]);
 	const addItem = (product: string) => {
 		console.log("MADE IT TO THE APP COMPONENT!");
-		console.log(product);
+		setItems([...items, { id: getId(), product, quantity: 1 }]);
 	};
 	// const items = [
 	// 	{ id: 1, product: "Lemon", quantity: 3 },
